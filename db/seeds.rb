849ebb6567d3883e14426a7222dac7446ec473ba
Aidi Stan <aidistan@live.com>
2017-12-17 23:59:34 +0800

    Update the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
+# There are two ways to populate the database:
+# - `db:seed` (or `db:setup`) will load the seed data here and is for production purpose
+# - `db:fixtures:load` will load all test fixutres and is only for development and test
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Create an initial administrator
+initial_admin = User.create(
+  mobile: '18812345678',
+  password: 'Pa55word',
+  password_confirmation: 'Pa55word'
+)
+
+# Create all hard-coded roles
+%w[_admin_ _staff_ user].each do |role|
+  initial_admin.roles << Role.create(name: role)
+end
+
+# Create one client at least
+Client.create(codename: 'mefit-web')
